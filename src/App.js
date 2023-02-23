06970c000b17575e8623faac65c535f9aab0b927
Abhishek Anand <anand2000ys@gmail.com>
2023-02-23 22:33:32 +0530

    structure ready

--- src/App.js
+++ src/App.js
@@ -3,7 +3,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <header className='header'>
+        My OCR app
+      </header>
+
+      <section className='hero'>
+        <label className='fileUploaderContainer'>
+          click here to upload images
+          <input type='file' id='fileUploader' multiple/>
+        </label>
+
+        <div>
+          {/* Previews will be shown here */}
+        </div>
+
+        <button className='button'>Generate</button>
+      </section>
+
+      <section className='results'>
+        <div className='results-result'>
+          <div className='results-result-image'>
+            <img width='250px' />
+          </div>
+          <div className='results-result-info'>
+            <div className='results-result-info-codes'>
+              <small></small>
+            </div>
+            <div className='results-result-info-codes'>
+              <small></small>
+            </div>
+            <div className='results-result-info-text'>
+              <small></small>
+            </div>
+          </div>
+          <hr />
+        </div>
+        <div className='results-result'>
+
+        </div>
+      </section>
     </div>
   );
 }
