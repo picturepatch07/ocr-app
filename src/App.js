1cb7b676ee44e80c6c9159e967f0281fee154b1c
Abhishek Anand <anand2000ys@gmail.com>
2023-02-25 22:20:33 +0530

    applying logic

--- src/App.js
+++ src/App.js
@@ -1,49 +1,63 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className='header'>
-        My OCR app
-      </header>
+class App {
+  constructor() {
 
-      <section className='hero'>
-        <label className='fileUploaderContainer'>
-          Choose an Image
-          <input type='file' id='fileUploader' multiple/>
-        </label>
+    render(); {
+      constructor(props); {
+        super(props);
+        this.state = {
+          uploads: [],
+          patterns: [],
+          documents: []
+        };
+      };
+    };
 
-        <div>
-          {/* Previews will be shown here */}
-        </div>
+    return (
+      <div className="App">
+        <header className='header'>
+          My OCR app
+        </header>
 
-        <button className='button'>Generate</button>
-      </section>
+        <section className='hero'>
+          <label className='fileUploaderContainer'>
+            Click here to upload documents
+            <input type='file' id='fileUploader' multiple />
+          </label>
 
-      <section className='results'>
-        <div className='results-result'>
-          <div className='results-result-image'>
-            <img width='250px' />
+          <div>
+            {/* Previews will be shown here */}
           </div>
-          <div className='results-result-info'>
-            <div className='results-result-info-codes'>
-              <small></small>
-            </div>
-            <div className='results-result-info-codes'>
-              <small></small>
-            </div>
-            <div className='results-result-info-text'>
-              <small></small>
-            </div>
-          </div>
-          <hr />
-        </div>
-        <div className='results-result'>
 
-        </div>
-      </section>
-    </div>
-  );
+          <button className='button'>Generate</button>
+        </section>
+
+        <section className='results'>
+          <div className='results-result'>
+            <div className='results-result-image'>
+              <img width='250px' />
+            </div>
+            <div className='results-result-info'>
+              <div className='results-result-info-codes'>
+                <small></small>
+              </div>
+              <div className='results-result-info-codes'>
+                <small></small>
+              </div>
+              <div className='results-result-info-text'>
+                <small></small>
+              </div>
+            </div>
+            <hr />
+          </div>
+          <div className='results-result'>
+
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
