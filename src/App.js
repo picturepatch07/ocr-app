f79186bc48b53651de897b90db1996b7d956d0b4
Abhishek Anand <anand2000ys@gmail.com>
2023-03-21 20:00:10 +0530

    files modified

--- src/App.js
+++ src/App.js
@@ -1,63 +1,11 @@
-import './App.css';
+import './App.css'
 
-class App {
-  constructor() {
-
-    render(); {
-      constructor(props); {
-        super(props);
-        this.state = {
-          uploads: [],
-          patterns: [],
-          documents: []
-        };
-      };
-    };
-
-    return (
-      <div className="App">
-        <header className='header'>
-          My OCR app
-        </header>
-
-        <section className='hero'>
-          <label className='fileUploaderContainer'>
-            Click here to upload documents
-            <input type='file' id='fileUploader' multiple />
-          </label>
-
-          <div>
-            {/* Previews will be shown here */}
-          </div>
-
-          <button className='button'>Generate</button>
-        </section>
-
-        <section className='results'>
-          <div className='results-result'>
-            <div className='results-result-image'>
-              <img width='250px' />
-            </div>
-            <div className='results-result-info'>
-              <div className='results-result-info-codes'>
-                <small></small>
-              </div>
-              <div className='results-result-info-codes'>
-                <small></small>
-              </div>
-              <div className='results-result-info-text'>
-                <small></small>
-              </div>
-            </div>
-            <hr />
-          </div>
-          <div className='results-result'>
-
-          </div>
-        </section>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Image to Text</h1>
+    </div>
+  )
 }
 
 export default App;
