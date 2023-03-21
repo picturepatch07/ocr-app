d0c9a684b4ab2d7170b5c3bb3d94af175f80d798
Abhishek Anand <anand2000ys@gmail.com>
2023-03-21 20:40:29 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+
   return (
-    <div className='App'>
-      <h1>Image to Text</h1>
+    <div className="container" style={{ height: "100vh" }}>
+      <div className="row h-100">
+        <div className="col-md-5 mx-auto dlflex flex-column align-items-center">
+          {!isLoading && <h1 className="mt-5 mb-4">Image to Text</h1>}
+
+          {/* form */}
+          {!isLoading && !text && (
+            <>
+              <input
+                type="file"
+                className="form-control"
+                onChange={(e) =>
+                  setImage(URL.createObjectURL(e.value.files[0]))
+                }
+              />
+            </>
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
